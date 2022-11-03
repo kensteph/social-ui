570bb95cba12b0b8fd3f7a3fe18dadd596ccc6fa
Kender ROMAIN <kenderromain@gmail.com>
2022-11-03 16:14:41 -0400

    Added Stories and Posts Components

---
+++ src/Components/stories/Stories.jsx
@@ -0,0 +1,25 @@
+import "./stories.scss";
+import { stories } from "../../data/dummy";
+import { useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+
+const Stories = () => {
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <div className="stories">
+      <div className="story">
+        <img src={currentUser.profilePicture} alt="" />
+        <span>{currentUser.name}</span>
+        <button>+</button>
+      </div>
+      {stories.map((story) => (
+        <div className="story">
+          <img src={story.img} alt="" />
+          <span>{story.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Stories;
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,11 @@
+import Posts from "../../Components/posts/Posts";
+import Stories from "../../Components/stories/Stories";
 import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
-      <span>HOME</span>
-      <h1>HOME</h1>
-      <h1>HOME</h1>
+     <Stories/>
+     <Posts/>
     </div>
   );
 };
